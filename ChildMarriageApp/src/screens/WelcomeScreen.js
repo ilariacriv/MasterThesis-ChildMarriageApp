e39872eaeba26583e2f239ff81d1c507b89ca59a
ilariacriv <58052642+ilariacriv@users.noreply.github.com>
2022-04-29 12:39:55 +0200

    feat-#2: styled welcome screen
    Closes #2

--- ChildMarriageApp/src/screens/WelcomeScreen.js
+++ ChildMarriageApp/src/screens/WelcomeScreen.js
@@ -1,15 +1,53 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
 import EmergencyCall from "../components/emergencyCall";
+import { VStack, Heading, Button, Box, Text } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const WelcomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
-      <View style={styles.container}>
+      <VStack
+        space={4}
+        marginTop="10"
+        width="100%"
+        height="100%"
+        bg="warning.50"
+        alignItems="center"
+      >
         <EmergencyCall />
-        <Text>Welcome!</Text>
-        <Button title="Start" onPress={() => navigation.navigate("Home")} />
-      </View>
+        <Heading size="2xl" alignSelf="center">
+          Welcome!
+        </Heading>
+        <Box
+          margin="15px"
+          bg="white"
+          borderColor="gray.300"
+          borderWidth="1"
+          width="5/6"
+          alignItems="center"
+          padding="20px"
+          height="3/6"
+          justifyContent="space-between"
+        >
+          <Text bold fontSize="md" paddingBottom="20px">
+            Tutorial here
+          </Text>
+          <MaterialCommunityIcons
+            name="arrow-right-bold-circle"
+            size={40}
+            color="black"
+          />
+        </Box>
+        <Button
+          onPress={() => navigation.navigate("Home")}
+          size="lg"
+          width="4/5"
+          text
+        >
+          Start
+        </Button>
+      </VStack>
     </SafeAreaView>
   );
 };
