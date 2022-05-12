fb7b20def98c43a474c2edeb83b25758d13f4af7
ilariacriv <58052642+ilariacriv@users.noreply.github.com>
2022-05-12 11:31:15 +0200

    fix-#31-#26: improved emergency number input
    Closes #31
    Closes  #26

--- ChildMarriageApp/src/screens/home/SettingsScreen.js
+++ ChildMarriageApp/src/screens/home/SettingsScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useContext } from "react";
 import {
@@ -10,6 +10,8 @@ import {
   Text,
   Radio,
   Input,
+  KeyboardAvoidingView,
+  ScrollView,
 } from "native-base";
 import HelpBar from "../../components/helpBar";
 import I18n from "../../i18n";
@@ -22,12 +24,17 @@ const SettingsScreen = ({ navigation }) => {
   const [localEN, setLocalEN] = React.useState(myContext.emergencyNumber);
 
   const onSave = () => {
-    myContext.updateLanguage(localLang);
-    if (localEN.length != 9) {
-      alert(I18n.t("settings.alertMessage"));
-      return;
+    if (localLang != myContext.language) {
+      myContext.updateLanguage(localLang);
     }
-    myContext.updateEmergencynumber(localEN);
+    if (localEN != myContext.emergencyNumber) {
+      var pattern = /^\d{9}$/;
+      if (!localEN.match(pattern)) {
+        alert(I18n.t("settings.alertMessage"));
+        return;
+      }
+      myContext.updateEmergencynumber(localEN);
+    }
     navigation.navigate("Home");
   };
   return (
@@ -38,58 +45,76 @@ const SettingsScreen = ({ navigation }) => {
           <Heading size="2xl" alignSelf="center">
             {I18n.t("common.settings")}
           </Heading>
-          <Box paddingY={5} alignItems="center">
-            <Text fontSize="xl" paddingBottom={3} bold>
-              {I18n.t("settings.lang")}:{" "}
-            </Text>
-            <Radio.Group
-              name="languageGroup"
-              accessibilityLabel="language"
-              value={localLang}
-              onChange={(nextValue) => {
-                setLocalLang(nextValue);
+          <ScrollView
+            _contentContainerStyle={{
+              justifyContent: "center",
+            }}
+          >
+            <Box paddingY={5} alignItems="center">
+              <Text fontSize="xl" paddingBottom={3} bold>
+                {I18n.t("settings.lang")}:{" "}
+              </Text>
+              <Radio.Group
+                name="languageGroup"
+                accessibilityLabel="language"
+                value={localLang}
+                onChange={(nextValue) => {
+                  setLocalLang(nextValue);
+                }}
+              >
+                <Radio value="en" my={1}>
+                  English
+                </Radio>
+                <Radio value="fr" my={1}>
+                  Français
+                </Radio>
+              </Radio.Group>
+            </Box>
+            <KeyboardAvoidingView
+              h={{
+                base: "400px",
+                lg: "auto",
               }}
+              behavior={Platform.OS === "ios" ? "padding" : "height"}
             >
-              <Radio value="en" my={1}>
-                English
-              </Radio>
-              <Radio value="fr" my={1}>
-                Français
-              </Radio>
-            </Radio.Group>
-          </Box>
-
-          <Box paddingBottom={3} alignItems="center">
-            <Text fontSize="xl" bold>
-              {I18n.t("settings.enum")}:{" "}
-            </Text>
-            <Text
-              fontSize="lg"
-              paddingBottom={3}
-              paddingX={3}
-              alignContent="center"
-            >
-              {I18n.t("settings.ecmess")}
-            </Text>
-            <Text
-              fontSize="lg"
-              paddingBottom={3}
-              paddingX={3}
-              alignContent="center"
-            >
-              {I18n.t("settings.curr")}: {myContext.emergencyNumber}
-            </Text>
-            <Input
-              mx="3"
-              placeholder={I18n.t("settings.placeholder")}
-              w="70%"
-              borderColor="black"
-              size="lg"
-              keyboardType="numeric"
-              onChangeText={(text) => setLocalEN(text)}
-            />
-          </Box>
-          <HStack paddingTop="5" space={3} style={styles.hstack}>
+              <Box paddingBottom={3} alignItems="center">
+                <Text fontSize="xl" bold>
+                  {I18n.t("settings.enum")}:{" "}
+                </Text>
+                <Text
+                  fontSize="lg"
+                  paddingBottom={3}
+                  paddingX={3}
+                  alignContent="center"
+                >
+                  {I18n.t("settings.ecmess")}
+                </Text>
+                <Text
+                  fontSize="lg"
+                  paddingBottom={3}
+                  paddingX={3}
+                  alignContent="center"
+                >
+                  {I18n.t("settings.curr")}: {myContext.emergencyNumber}
+                </Text>
+                <Input
+                  mx="3"
+                  placeholder={I18n.t("settings.placeholder")}
+                  w="70%"
+                  borderColor="black"
+                  size="lg"
+                  keyboardType="numeric"
+                  onChangeText={(text) => setLocalEN(text)}
+                />
+              </Box>
+            </KeyboardAvoidingView>
+          </ScrollView>
+          <HStack
+            paddingTop="3"
+            space={3}
+            style={styles.hstack}
+            paddingBottom="3"
+          >
             <Button
               onPress={() => navigation.navigate("Home")}
               size="lg"
